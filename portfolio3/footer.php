be387084d3da9597ff06ac32e5723e1fe4ef2b73
ju-clt-50 <j.clouet@mancheopenschool.fr>
2017-04-05 17:04:24 +0200

    travaux 5 avril

---
+++ portfolio3/footer.php
@@ -0,0 +1,6 @@
+<div class="contentfooter">
+  <p1>Crée par Julien CLOUET , tout droits réservés ©</p1>
+  <p2>mentions légales</p2>
+      <i class="fa fa-twitter fa-3x"></i>
+      <i class="fa fa-facebook fa-3x" href="https://twitter.com/ju_clt_50"></i>
+  </div>
--- portfolio3/nav.php
+++ portfolio3/nav.php
@@ -1,9 +1,10 @@
+<!--- content of navbar --->
 <div class="topnav" id="mytopnav">
-    <img src="Wisna_logo.jpg">
+    <img src="images/logowisna_header.png">
       <a href="#accueil">Accueil</a>
       <a href="#quiSommesNous">Qui-sommes-nous?</a>
       <a href="#Wisna">Wisna</a>
       <a href="#projets">Projets</a>
       <a href="#contact">Contact</a>
-      <a href="wiki.php">Wiki</a>
+      <a href="#partenaires">Partenaires</a>
 </div>
